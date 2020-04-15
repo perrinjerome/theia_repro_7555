ae2bd1ed2d549a0e5f9686a836d7765b9a37022e
Jérome Perrin <perrinjerome@gmail.com>
2020-04-15 04:45:07 +0000

    a bit better build script

--- build.sh
+++ build.sh
@@ -1,20 +1,28 @@
-
-set -e
-
+# show what we use
+echo "We are using:"
 git version
-env | grep GIT || :
+echo GIT_EXEC_PATH= $GIT_EXEC_PATH
+sleep 1
 
-
-cd /tmp
-INSTALL_DIR=$(pwd)/git-install
+INSTALL_DIR=/tmp/git-install
 mkdir -p $INSTALL_DIR
 
-curl -L https://www.kernel.org/pub/software/scm/git/git-2.26.0.tar.xz  | tar -xJf -
-cd git-2.26.0/
+(
+    # build another git version
+    cd /tmp
+    curl -L https://www.kernel.org/pub/software/scm/git/git-2.24.0.tar.xz  | tar -xJf -
+    cd git-2.24.0/
+    ./configure --prefix=$INSTALL_DIR
+    make -j 10
+    make install
+) > build.log 2>&1
 
-./configure --prefix=$INSTALL_DIR
-make -j 10
-make install
-
+# use the git we just build
 cd /tmp
 $INSTALL_DIR/bin/git clone --recursive https://github.com/eclipse-theia/theia
+echo "... this did not work, because of the incompatible GIT_EXEC_PATH"
+echo
+
+echo "if we unset GIT_EXEC_PATH, then no problem:"
+rm -rf theia
+GIT_EXEC_PATH= $INSTALL_DIR/bin/git clone --recursive https://github.com/eclipse-theia/theia
